25de2084c55eb334b135f2269d4a14f90f17ba85
Armand XU <ziyi_xu@icloud.com>
2022-08-04 03:19:11 +0200

    update gitignore

---
+++ src/routes/implementations/public-apis/Foodish.svelte
@@ -0,0 +1,20 @@
+<script context="module">
+    export async function load({ fetch }) {
+        const resp = await fetch(`https://foodish-api.herokuapp.com/api/`);
+        const resp_json = await resp.json()
+        return {
+            props: {
+                data: resp_json
+            }
+        }
+    }
+</script>
+
+
+<script>
+    export let data;
+</script>
+
+<h1>
+    TEST {data}
+</h1>
